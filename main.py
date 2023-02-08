c19a4ba11d9c95c81b167d89a85e498da87254fc
Eplutt <83497967+Eplutt@users.noreply.github.com>
2023-02-08 18:57:17 +0100

    update

--- main.py
+++ main.py
@@ -2,48 +2,190 @@ import os
 #modul
 import inventory
 from time import sleep
-if __name__ == "__main__":
+import tictactoe
+
+
+#Deklaration av variabler
+############################################################################################################
+
+current_room = 0
+
+controls = '''
+
+Kontrollerna för att spela:
+Skriv [kolla] för att kolla dig omkring i rummet,
+[inspektera] för att titta närmare på eventuella föremål i rummen,
+[i] för att kolla vilka föremål du har(inventory),
+[hjälp] för att se kontrollerna igen,
+
+   '''
+backstory = 'Du festade sent igår på en klubb. Du mådde inte så bra så att du försökte ta dig hem men du var för påverkad för att köra så du gick hem. På vägen hem ser du din farbror i en bil. Du frågar om en skjuts hem. Sen får du ett slag mot huvudet och vaknar i ett kolsvart rum. Bredvid dig finns det en ficklampa. Då inser du att du är i en källare...'
+
+#Deklration av funktioner
+############################################################################################################
+
+def inspect(item):
+   'returnerar en beskrivning av föremålet om det finns'
+
+   #Går igenom listan med item info och kollar om föremålet finns med
+   for i in item_info:
+      if item == i:
+         return item_info[item]
+
+      elif item == '' or item == ' ':
+         return f'Det specifierade inte vad du ville inspektera. '
+
+
+      else:
+         return f'Det finns ingen {item} i det här rummet. '
+
+
+def split_line_print(text, seperator, sleep_tine, end='\n'):
+   'Splittar "text" vid "seperator" och skriver ut en bit per rad med sleep_time tid emellan'
+   for i in text.split(seperator):
+      print(i, end=end)
+      sleep(sleep_tine)
+
+
+def events(event):
+   ''
+   print('Råttan utmanar dig till ett parti 3-i-rad. \nIfall du vinner säger den att du ska få nyckeln till källardörren.')
+   while True:
+      if 'rat' in event:
+         player_input = input('Är du redo? Ja/Nej').lower()
+         if 'ja' in player_input:
+            if tictactoe.play(tictactoe.player, tictactoe.win) == True:
+               break
+
+         elif 'nej' in player_input:
+            print('Inspektera hinken igen när du är redo')
+            break
+#Deklration av listot, dictionaries osv
+############################################################################################################
+
+rooms = [
+  'källaren', 'trappan', 'vardagsrummet', 'hallen', 'sovrummet', 'köket',
+  'garaget'
+]
+
+room_descriptions = {
+  rooms[0]:
+  'Du kollar runt och ser att du är i en gammal mörk källare. I källaren ser du en tavla och en hink på golvet. Det finns en dörr som leds ut ur rummet.',
+  rooms[1]: 'Trappan',
+  rooms[2]: 'Vardagsrummet',
+  rooms[3]: 'Hallen',
+  rooms[4]: 'Sovrummet',
+  rooms[5]: 'Köket',
+  rooms[6]: 'Garaget'
+}
+
+
+item_info = {
+  'hink' : 'Hinken ser gammaö och rostig ut. I den hittar en råtta. event rat',
+  '': '',
+}
+
+
+
+############################################################################################################
+
+
+
+
+
+if __name__ == '__main__':
    os.system('cls')
 
-   print('''
-                  Välkommen till
-   _  _  (_)(_)  __    __      __    ____  ____  _  _ 
-   ( )/ )  /__\  (  )  (  )    /__\  (  _ \( ___)( \( )
-   )  (  /(__)\  )(__  )(__  /(__)\  )   / )__)  )  ( 
-   (_)\_)(__)(__)(____)(____)(__)(__)(_)\_)(____)(_)\_)''')
+   split_line_print(backstory, '. ', 1.3)
 
-   print('''
+   sleep(2)
+   os.system('cls')
+   sleep(0.1)
+
+   split_line_print('''
+                  Välkommen till
+   _  _  (_)(_)  __    __      __    ____  ____  _  _ §
+   ( )/ )  /__\  (  )  (  )    /__\  (  _ \( ___)( \( )§
+   )  (  /(__)\  )(__  )(__  /(__)\  )   / )__)  )  ( §
+   (_)\_)(__)(__)(____)(____)(__)(__)(_)\_)(____)(_)\_)§''', '§', 0.1, '')
+
+   sleep(0.25)
+
+   split_line_print('''
                                                 
                                                 
                                                 
-            ................                  
-         ......*.,.,,,.,.,......              
-      ........,,.,,,,,,,,...........          
-      .....,,,,,**,,(.ma (.          , .        
-      ......,,,,,***(,.((/,        * .        
-      .....,,,,,,***(. (*//*.      * ...      
-      ....,,,,,,,***(. (*//*.      / ...      
-      ......,,,,,,**(. (*//*.      / ....     
-         .......,,,,,,(. (*//*.      * .....    
-         ....,,,,,,,/. (*//*. *    * .....    
-            ....,.,,,/. ((//*/.     * ...      
-            .. .......*. ((//*.      , ...      
-               ......*. ((//*.      , ..       
-                  ...,. */**..      .          
-                     ................          
-   ''')
+            ................                  §
+         ......*.,.,,,.,.,......              §
+      ........,,.,,,,,,,,...........          §
+      .....,,,,,**,,(.ma (.          , .       § 
+      ......,,,,,***(,.((/,        * .        §
+      .....,,,,,,***(. (*//*.      * ...      §
+      ....,,,,,,,***(. (*//*.      / ...      §
+      ......,,,,,,**(. (*//*.      / ....     §
+         .......,,,,,,(. (*//*.      * .....   § 
+         ....,,,,,,,/. (*//*. *    * .....    §
+            ....,.,,,/. ((//*/.     * ...      §
+            .. .......*. ((//*.      , ...      §
+               ......*. ((//*.      , ..       §
+                  ...,. */**..      .          §
+                     ................          §
+   ''', '§', 0.1, '')
+
+   sleep(0.5)
+
 
 
    while True:
-      start = input("\nStart\nAvslut\n").lower()
-      if "avslut" in start or start == "a":
+      start = input('\nStart\nAvslut\n').lower()
+      if 'avslut' in start or start == 'a':
          exit()
 
-      elif "start" in start or start == "s":
+      elif 'start' in start or start == 's':
+         os.system('cls')
          break
 
-   print("")
+
+
+
+
+   print(controls)
 
    while True:
-      print("a")
-      response = input()
+      #if - delar upp spelarens input vid första mellanrummet så att man kan använda resten för att förstå hur spelaren vill utföra ett kommando
+      #elif - kollar ifall spelaren bara klickat på enter
+      #else - gör om player_input till en lista med ett tomt element. Förhindrar krashar
+      player_input = input().lower()
+      if ' ' in player_input:
+         player_input = player_input.split(' ', 1)
+      elif player_input == '':
+         continue
+      else:
+         player_input = [player_input, '']
+
+
+      print(player_input)
+      #Kommandon
+      ######################################################################################################
+      commands = {
+      'kolla': room_descriptions[rooms[current_room]],
+      'hjälp' : controls,
+      'inspektera': inspect(player_input[1]),
+      #'i' or 'väska': inventory(),
+      
+      }
+
+
+      ######################################################################################################
+
+      if player_input[0] in commands:
+         if 'event' in commands[player_input[0]]:
+            player_input = commands[player_input[0]].split('event')
+            print(player_input[0])
+            events(player_input[1])
+         else:   
+            print(commands[player_input[0]])
+
+
+      else:
+         print('Det där var inget kommando. Skriv [hjälp] om du vill se vilka komandon som finns igen. ')
--- tictactoe.py
+++ tictactoe.py
@@ -1,88 +1,98 @@
-import os    
-import time    
-    
-board = [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']    
-player = 1    
-   
-########win Flags##########    
-win = 1    
-draw = -1    
-running = 0    
-stop = 1    
-###########################    
-game = running    
-mark = 'X'    
+import os
+import random
+
+
+board = [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']
+player = 1
+
+
+win = 1
+draw = -1
+running = 0
+stop = 1
+
+
+
+game = running
+mark = 'X'
    
 
-def draw_board():    
+def draw_board():
     'Målar spelbrädan'
-    print(" %c | %c | %c " % (board[1],board[2],board[3]))    
-    print("___|___|___")    
-    print(" %c | %c | %c " % (board[4],board[5],board[6]))    
-    print("___|___|___")    
-    print(" %c | %c | %c " % (board[7],board[8],board[9]))    
-    print("   |   |   ")    
+    print(' %c | %c | %c ' % (board[1],board[2],board[3]))
+    print('___|___|___')
+    print(' %c | %c | %c ' % (board[4],board[5],board[6]))
+    print('___|___|___')
+    print(' %c | %c | %c ' % (board[7],board[8],board[9]))
+    print('   |   |   ')
    
 
 def check_position(x):
-    'Kollar ifall det redan är något i rutan '    
-    if(board[x] == ' '):    
-        return True    
-    else:    
-        return False    
+    'Kollar ifall det är något i rutan '
+    if(board[x] == ' '):
+        return True
+    else:
+        return False
    
 
-def check_win():    
+def check_win():
     'Kollar om någon vunnit'
-    global game    
-    #Horizontal winning condition    
-    if(board[1] == board[2] and board[2] == board[3] and board[1] != ' '):    
-        game = win    
-    elif(board[4] == board[5] and board[5] == board[6] and board[4] != ' '):    
-        game = win    
-    elif(board[7] == board[8] and board[8] == board[9] and board[7] != ' '):    
-        game = win    
-    #Vertical winning Condition    
-    elif(board[1] == board[4] and board[4] == board[7] and board[1] != ' '):    
-        game = win    
-    elif(board[2] == board[5] and board[5] == board[8] and board[2] != ' '):    
-        game = win    
-    elif(board[3] == board[6] and board[6] == board[9] and board[3] != ' '):    
-        game=win    
-    #Diagonal winning Condition    
-    elif(board[1] == board[5] and board[5] == board[9] and board[5] != ' '):    
-        game = win    
-    elif(board[3] == board[5] and board[5] == board[7] and board[5] != ' '):    
-        game=win    
-    #Match Tie or draw Condition    
-    elif(board[1]!=' ' and board[2]!=' ' and board[3]!=' ' and board[4]!=' ' and board[5]!=' ' and board[6]!=' ' and board[7]!=' ' and board[8]!=' ' and board[9]!=' '):    
-        game=draw    
-    else:            
-        game=running    
-    
- 
-while(game == running):    
-    os.system('cls')    
-    draw_board()    
-    if(player % 2 != 0):    
-        print("Player 1's chance")    
-        mark = 'X'    
-    else:    
-        print("Player 2's chance")    
-        mark = 'O'    
-    choice = int(input("Enter the position between [1-9] where you want to mark : "))    
-    if(check_position(choice)):    
-        board[choice] = mark    
-        player+=1    
-        check_win()    
-    
-os.system('cls')    
-draw_board()    
-if(game==draw):    
-    print("Oavgjort")    
-elif(game==win):    
-    player-=1    
-    if(player%2!=0):    
-        print("Du vann!")    
-    else:    
-        print("Du förlorade.")  
+    global game
+
+#Horisontel vinst
+    if(board[1] == board[2] and board[2] == board[3] and board[1] != ' '):
+        game = win
+    elif(board[4] == board[5] and board[5] == board[6] and board[4] != ' '):
+        game = win
+    elif(board[7] == board[8] and board[8] == board[9] and board[7] != ' '):
+        game = win
+#Vertikal vinst
+    elif(board[1] == board[4] and board[4] == board[7] and board[1] != ' '):
+        game = win
+    elif(board[2] == board[5] and board[5] == board[8] and board[2] != ' '):
+        game = win
+    elif(board[3] == board[6] and board[6] == board[9] and board[3] != ' '):
+        game=win
+#Diagonal vinst
+    elif(board[1] == board[5] and board[5] == board[9] and board[5] != ' '):
+        game = win
+    elif(board[3] == board[5] and board[5] == board[7] and board[5] != ' '):
+        game=win
+#Oavgjort
+    elif(board[1]!=' ' and board[2]!=' ' and board[3]!=' ' and board[4]!=' ' and board[5]!=' ' and board[6]!=' ' and board[7]!=' ' and board[8]!=' ' and board[9]!=' '):
+        game=draw
+    else:
+        game=running
+
+def play(player, win):
+    'startar tre-i-rad'
+    while(game == running):
+        os.system('cls')
+        draw_board()
+        if(player % 2 != 0):
+            print('Din tur')
+            mark = 'X'
+            choice = int(input('Vart vill du placera? 1-9\n'))
+        else:
+            print('Råttan')
+            mark = 'O'
+            choice = random.randint(1,9)
+        if(check_position(choice)):
+            board[choice] = mark
+            player+=1
+            check_win()
+
+    os.system('cls')
+    draw_board()
+    if(game==draw):
+        print('Oavgjort')
+    elif(game==win):
+        player-=1
+        if(player%2!=0):
+            print('Du vann!\nDu får nu nyckeln')
+            return True
+        else:
+            print('Du förlorade.\nDu kan köra igen.')  
+
+if __name__ == '__main__':
+    play(player, win)
